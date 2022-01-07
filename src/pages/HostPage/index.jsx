aa767c1a8d2e34bdeb0b620d7b66d5d2e88a2da0
changyuyeo <wry5533@gmail.com>
2022-01-08 01:06:41 +0900

    Host router 수정

--- src/pages/HostPage/index.jsx
+++ src/pages/HostPage/index.jsx
@@ -1,13 +1,12 @@
-import { lazy, useEffect } from 'react'
+import { useEffect } from 'react'
 import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
 
 import HostLayout from 'components/layout/HostLayout'
-
-const HostInit = lazy(() => import('pages/HostPage/HostInit'))
-const HostBank = lazy(() => import('pages/HostPage/HostBank'))
-const HostNum = lazy(() => import('pages/HostPage/HostNum'))
-const HostEmail = lazy(() => import('pages/HostPage/HostEmail'))
-const HostSuccess = lazy(() => import('pages/HostPage/HostSuccess'))
+import HostInit from 'pages/HostPage/HostInit'
+import HostBank from 'pages/HostPage/HostBank'
+import HostNum from 'pages/HostPage/HostNum'
+import HostEmail from 'pages/HostPage/HostEmail'
+import HostSuccess from 'pages/HostPage/HostSuccess'
 
 // 가격(init) -> 은행선택(bank) -> 계좌입력(num) -> 닌텐도 아이디(email) -> 종료
 const HostPage = () => {
