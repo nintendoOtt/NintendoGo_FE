0f1038ab3dac8da58bcd34b286adf13e930273f3
minu <gkqkehs10@naver.com>
2022-01-21 20:03:58 +0900

    delete redux & saga

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react'
-import { Provider } from 'react-redux'
 
 import RouterConfig from 'router'
 import theme from 'styles/theme'
 import GlobalStyle from 'styles/GlobalStyle'
 import LoadingSpinner from 'components/common/LoadingSpinner'
-import configureStore from './store/configureStore'
 import './App.css'
 
 const App = () => (
 	<>
-		<Provider store={configureStore()}>
-			<BrowserRouter>
-				<ThemeProvider theme={theme}>
-					<GlobalStyle />
-					<Suspense fallback={<LoadingSpinner />}>
-						<RouterConfig />
-					</Suspense>
-				</ThemeProvider>
-			</BrowserRouter>
-		</Provider>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Suspense fallback={<LoadingSpinner />}>
+					<RouterConfig />
+				</Suspense>
+			</ThemeProvider>
+		</BrowserRouter>
 	</>
 )
 
--- src/apollo/pay.js
+++ src/apollo/pay.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client'
 
 export const GUEST_PAY = gql`
     query($userId: String!, $nintendoId: String!) {
-        GuestPay(userId: $userId, nintendoId: $nintendoId) {
+        GuestPay(userId: $userId, token: $token, nintendoId: $nintendoId) {
             userId
             payId
             price
